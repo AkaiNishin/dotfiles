b9332dccf740a0506bb983237178dc35bcc96107
AkaiNishin <coreajustin288@gmail.com>
2024-11-09 20:59:59 -0600

    Added bookmarks
    remove bookmarks without memory and projects for a bookmark with memory

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,24 +1,19 @@
 return {
     require("full-border"):setup(),
     require("git"):setup(),
-    require("projects"):setup({
-        save = {
-            method = "yazi", -- yazi | lua
-            lua_save_path = "~/.config/yazi/state/projects.json"
-        },
-        last = {
-            update_after_save = true,
-            update_after_load = true,
-        },
-        merge = {
-            quit_after_merge = false,
-        },
-        notify = {
-            enable = true,
-            title = "Projects",
-            timeout = 3,
-            level = "info",
-        },
+    require("simple-mtpfs"):setup({}),
+   require("yamb"):setup({
+        -- Optional, the path ending with path seperator represents folder.
+        bookmarks = bookmarks,
+        -- Optional, recieve notification everytime you jump.
+        jump_notify = true,
+        -- Optional, the cli of fzf.
+        cli = "fzf",
+        -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+        keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        -- Optional, the path of bookmarks
+        path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or 
+            (os.getenv("HOME") .. "/.config/yazi/bookmark"),
     })
 }
 
