45420cc516db73333123ff56c5c397b00fa34989
AkaiNishin <coreajustin288@gmail.com>
2024-11-10 10:39:52 -0600

    retaltivenumber
    add relative number config. add new keybing for split and python venv
    path

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
@@ -8,9 +9,11 @@ vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
-vim.keymap.set("n", "<C-v>", ":vsplit<CR>")
+vim.keymap.set("n", "<C-|>", ":vsplit<CR>")
 vim.keymap.set("n", "<C-->", ":split<CR>")
+
 vim.g.python3_host_prog = os.getenv("HOME") .. "/Programming/venv/bin/python"
 require("config.lazy")
+
 vim.opt.tags:append('tags')
 vim.cmd.colorscheme("catppuccin-mocha")
