eb3642c9eb086cdd338658d0569c3c507501b2d8
AkaiNishin <coreajustin288@gmail.com>
2024-11-09 21:15:27 -0600

    nvim
    add neovim to stow

--- neovim/.config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd("set number")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-
-require("config.lazy")
-vim.opt.tags:append('tags')
-vim.cmd.colorscheme("catppuccin-mocha")
