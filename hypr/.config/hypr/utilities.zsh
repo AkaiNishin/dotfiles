226891b093fd18e940674abf5a0eb493bc5d3c90
AkaiNishin <coreajustin288@gmail.com>
2024-11-14 07:55:33 -0600

    browserExtras
    added and improved browser new tab from keyboard

---
+++ hypr/.config/hypr/utilities.zsh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+STREAMERS=("banished_pheasant" "MaruFideos" "Meica05" "TheKasaneru", "TipitoEnojado")
+SELECTED=$(printf "%s\n" "${STREAMERS[@]}" | rofi -dmenu -p "Select twitch streamer: ")
+
+[ -z "$SELECTED" ] && exit
+
+mpv --title="Twitch: $SELECTED" "https://www.twitch.tv/$SELECTED" &
+
+sleep 2
+hyprctl dispatch focuswindow "Twitch: $SELECTED"
+hyprctl dispatch pin active
