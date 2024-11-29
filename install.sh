eb497a5d57d3a5d32f3886f61b25083f688c6b37
AkaiNishin <coreajustin288@gmail.com>
2024-11-28 22:14:09 -0600

    more inicialization system

--- install.sh
+++ install.sh
@@ -3,39 +3,43 @@ echo 'Initializing system'
 read -p "First run of install proccess?: (y/n)" firstRun
 
 if [ "$firstRun" == "y" ]; then
+    echo "Installing pacman packages"
     cd "$HOME/dotfiles"
     sudo pacman -Syu
     ### install config files minimun ###
-    sudo pacman -S --needed stow
-    sudo pacman -S --needed kitty fastfetch tmux
-    sudo pacman -S --needed hyprland hyprpaper hyprcursor waybar rofi
-    sudo pacman -S --needed neovim
+    sudo pacman -S --needed --noconfirm stow kitty fastfetch tmux neovim
+    sudo pacman -S --needed --noconfirm hyprland hyprpaper hyprcursor waybar rofi
     ### development ### 
-    sudo pacman -S --needed clang cmake git lazygit ctags python stlink minicom
+    sudo pacman -S --needed --noconfirm clang cmake git lazygit ctags python stlink minicom gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
     #libraries
-    sudo pacman -S --needed ncurses sfml
+    sudo pacman -S --needed --noconfirm ncurses sfml
     ### basic stuff necessary ###
-    sudo pacman -S --needed firefox flatpak mpv yazi
+    sudo pacman -S --needed --noconfirm firefox flatpak mpv yazi texlive
     ### utilies ####
-    sudo pacman -S --needed zsh bat btop dunst cowsay fzf gnuplot yay zoxide
+    sudo pacman -S --needed --noconfirm zsh bat btop dunst cowsay fzf gnuplot zoxide zip, unzip vlc noto-fonts-cjk fcitx5-mozc noto-fonts-emoji
     ### sistem requeriments ###
-    sudo pacman -S --needed base-devel binutils xwaylandvideobridge
+    sudo pacman -S --needed --noconfirm base-devel binutils ttf-meslo-nerd cargo npm
 
     ### setup enviroment ###
     # Install yay from AUR
+    echo "Installing yay packages"
     git clone https://aur.archlinux.org/yay.git "$HOME/yay"
     cd "$HOME/yay" || exit
     makepkg -si --noconfirm
     cd "$HOME" || exit
     rm -rf "$HOME/yay"
     
-    yay -S --noconfirm wlogout hyprshot hyprpicker visual-studio-code-bin pavucontrol
-
+    yay -S --needed --noconfirm wlogout hyprshot hyprpicker visual-studio-code-bin pavucontrol xdg-desktop-portal-hyprland-git
     
     # init python venv
+    echo "Inicializing python virtual venv enviroment...Installing libraries"
     mkdir -p "$HOME/Programming/"
     python -m venv "$HOME/Programming/venv"
+    source ~/Programming/venv/bin/activate
+    pip install numpy pandas scipy mypy pyinstaller customtkinter pygame pillow scikit-learn
+    deactivate
     
+    echo "cloning repositories...utilities"
     # init zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     # zsh plugins
@@ -47,8 +51,23 @@ if [ "$firstRun" == "y" ]; then
     git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
     mkdir -p "$HOME/.config/tmux/plugins/catppuccin"
     git clone -b v2.1.1 https://github.com/catppuccin/tmux.git "$HOME/.config/tmux/plugins/catppuccin/tmux"
+    
+    # init sdk for pico
+    mkdir -p "$HOME/Programming/pico"
+    cd "$HOME/Programming/pico"
+    if [ ! -d "pico-sdk" ]; then
+        git clone https://github.com/raspberrypi/pico-sdk.git --branch master
+    fi
+    cd "pico-sdk"
+    git submodule update --init
+    cd ../
+    if [ ! -d "pico-examples" ]; then
+    git clone https://github.com/raspberrypi/pico-examples.git --branch master
+    fi
+    cd "$HOME/dotfiles/"
 
     # link dotfiles with stow
+    echo "linking dotfiles"
     cd "$HOME/dotfiles"
     stow fastfetch
     stow hypr
@@ -75,7 +94,6 @@ elif [ "$firstRun" == "n" ]; then
     flatpak install -y flathub com.valvesoftware.Steam
     flatpak install -y flathub dev.vencord.Vesktop
     flatpak install -y flathub org.libreoffice.LibreOffice
-    flatpak install -y flathub org.videolan.VLC
     flatpak install -y flathub org.telegram.desktop
     flatpak install -y flathub com.mattjakeman.ExtensionManager
     flatpak install -y flathub org.kde.krita
